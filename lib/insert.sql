346384e1ea324327810da96be74382e119a332ad
deborahchepkoech <deborah.chepkoech@student.moringaschool.com>
2025-05-21 20:17:22 +0300

    Organized bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'F', 'brown', 'happy', 1),
+('Rowdy', 20, 'M', 'yellow', 'sad', 1),
+('Tabitha', 15, 'F', 'black', 'aggressive', 1),
+('Seargeant Brown', 50, 'M', 'pink', 'moody', 0),
+('Melissa', 30, 'F', 'lilac', 'joyful', 1),
+('Grinch', 40, 'M', 'violet', 'angry', 1),
+('Wendy', 26, 'F', 'blue', 'warm', 1),
+(NULL, 1, 'M', 'purple', 'curious', 1);
